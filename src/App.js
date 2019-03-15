ebd44433193b04107b9af58962eddd79dcc68524
Christopher W. Brown <christopherwilliambrown@gmail.com>
2019-03-15 10:24:37 -0400

    Add React Files
    Add simple functional component in app.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>
+  )
+}
+
+export default App;
