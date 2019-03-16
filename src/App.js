990782387166c8c91ed18a79d2da7602e46c3469
Christopher W. Brown <christopherwilliambrown@gmail.com>
2019-03-16 13:57:19 -0400

    Add markup, switch to class based component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-  <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-  </ul>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div> 
+        
+        <div id="quote-box" className="post card">
+          <div id="text"> 
+          </div>  
+          <div id="author"> Author
+          </div>
+          <div id="new-quote"> New Qoute
+          </div>  
+          <div id="tweet-quote"> Tweet quote
+          </div>  
+        </div>
+       </div>
+      )
+    
+  }
 }
 
 export default App;
