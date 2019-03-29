f58d33133476965fb186a4fb27512efd91ba1d13
Christopher W. Brown <christopherwilliambrown@gmail.com>
2019-03-29 11:03:26 -0400

    Remove old data array, comment out redundant code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const quotes = [
-  {id:1,
-   text: 'You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.”.',
-  author: 'Dr. Suess'},
-  {id:2,
-   text: 'Live in the sunshine, swim the sea, drink the wild air.',
-  author: 'Ralph Waldo Emerson'},
-  {id:3,
-   text: 'The successful warrior is the average man, with laser-like focus.',
-  author: 'Bruce Lee'}
-  
-];
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
-     super()
-     this.state = {
-     id: quotes.id,
-     text: quotes[0].text,
-     author: quotes[0].author,
-     quotesData: []
-      
-    }
-   this.handleClick = this.handleClick.bind(this)
+    super();
+    this.state = {
+      quotesData: []
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  /*
    componentDidMount() {
      fetch('./data.json')
      .then(response => response.json())
@@ -47,62 +31,54 @@ class App extends React.Component {
   }) 
   
 }
+*/
+  handleClick() {
+    fetch("./data.json")
+      .then(response => response.json())
+      .then(result => {
+        const myQuotes = result.map(item => {
+          return item;
+        });
 
-handleClick() {
-  fetch('./data.json')
-  .then(response => response.json())
-  .then(result => {
- const myQuotes = result.map(item => {
-   return item
- })
- 
- this.setState ({
-   quotesData: myQuotes
- })
- const randQuote = this.state.quotesData[ Math.floor(Math.random() * this.state.quotesData.length)]
- console.log(randQuote)
- this.setState ({
-   quotesData: randQuote
- })
-}) 
+        this.setState({
+          quotesData: myQuotes
+        });
+        const randQuote = this.state.quotesData[
+          Math.floor(Math.random() * this.state.quotesData.length)
+        ];
+        this.setState({
+          quotesData: randQuote
+        });
+      });
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1 className="row justify-content-center"> Random Quote Generator </h1>
+        <div id="quote-box" className="post card ">
+          <div id="text" className="row justify-content-center">
+            {this.state.quotesData.quoteText}
+          </div>
+          <div id="author" className="row justify-content-center">
+            {this.state.quotesData.quoteAuthor}
+          </div>
+          <div id="new-quote" className="row justify-content-center">
+            <button
+              onClick={this.handleClick}
+              type="button"
+              className="btn btn-primary">
+              Show Me A New Quote
+            </button>
+          </div>
 
-}
-
-   
-   
-   
-   render() {
-     
-     return (
-       <div className="container"> 
-         <h1 className="row justify-content-center"> Random Quote Generator </h1>
-         <div id="quote-box" className="post card ">
-          
-           <div id="text" className="row justify-content-center"> 
-           {this.state.quotesData.quoteText}
-              
-             </div>  
-             <div id="author" className="row justify-content-center"> 
-                {this.state.quotesData.quoteAuthor}
-                </div>
-                
-          
-           <div> </div>
-           <div id="new-quote" className="row justify-content-center">
-             <button 
-               onClick={this.handleClick}
-               type="button" 
-               className="btn btn-primary">New Quote</button>
-           </div> 
-           
-           <div id="tweet-quote" className="row justify-content-center" > 
-             <button type="button" className="btn btn-info">Tweet quote </button>
-           </div>
-           
-         </div>
+          <div id="tweet-quote" className="row justify-content-center">
+            <button type="button" className="btn btn-info">
+              Tweet quote{" "}
+            </button>
+          </div>
         </div>
-       )
-     
-   }
- }
+      </div>
+    );
+  }
+}
 export default App;
